ff4839926c42395eb3dd501473cc89bb583f27a9
cristiano.lima <crisbypass@outlook.com>
2024-10-17 20:03:12 -0300

    Start

--- EclipseWorksChallenge/Program.cs
+++ EclipseWorksChallenge/Program.cs
@@ -1,6 +1,100 @@
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<MyMvc.Identity_Data.ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+
+//builder.Services
+//.AddControllers()
+//.ConfigureApiBehaviorOptions(options =>
+//{
+//    options.SuppressMapClientErrors = true;
+//    options.SuppressModelStateInvalidFilter = true;
+//});
+
+var tokenKey = "MyUltraUnbelievableSecretKey@#$10";
+var key = Encoding.ASCII.GetBytes(tokenKey);
+
+//builder.Services.AddAuthentication(x =>
+//{
+//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//.AddJwtBearer(x =>
+//{
+//    x.RequireHttpsMetadata = false;
+//    x.SaveToken = true;
+//    x.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuerSigningKey = true,
+//        IssuerSigningKey = new SymmetricSecurityKey(key),
+//        ValidateIssuer = false,
+//        ValidateAudience = false
+//    };
+//    x.Events = new JwtBearerEvents
+//    {
+//        OnMessageReceived = context =>
+//        {
+//            var accessToken = context.Request.Query["access_token"];
+//            // If the request is for our hub(to SignalR, in fact)...
+//            var path = context.HttpContext.Request.Path;
+//            if (!string.IsNullOrEmpty(accessToken) &&
+//                path.StartsWithSegments("/MySignalR/MyHub"))
+//            {
+//                // Read the token out of the query string
+//                context.Token = accessToken;
+//            }
+//            return Task.CompletedTask;
+//        }
+//    };
+//});
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("MyHubPolicy2", policy =>
+//    {
+//        policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+//        policy.RequireAuthenticatedUser();
+
+//        //policy.RequireAuthenticatedUser();
+//        policy.RequireRole("Banta");
+//        //policy.RequireRole("Panta");
+//    });
+
+//});
+
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+//        new OpenApiSecurityScheme
+//        {
+//            Description = "Please, provide token value.",
+//            In = ParameterLocation.Header,
+//            Name = "Authorization",
+//            Type = SecuritySchemeType.Http,
+//            BearerFormat = "JWT",
+//            Scheme = "bearer"
+//        });
+
+//    options.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                {
+//                    Type = ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            Array.Empty<string>()
+//        }
+//    });
+//});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,10 +110,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+//app.UseHttpsRedirection();
+
+//app.UseRouting();
+
+//app.UseCors();
+
+//app.UseAuthentication();
+
+//app.UseAuthorization();
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
---
+++ MyTests/UnitTests.cs
@@ -0,0 +1,43 @@
+namespace MyTests
+{
+    public class UnitTests
+    {
+        //[Fact]
+        //public void MyFirstMethod()
+        //{
+        //    var b = MyClass.MyBoolMethod(true);
+        //    Assert.True(b);
+        //}
+
+        //[InlineData([false])]
+        //[InlineData([true])]
+        //[InlineData([null])]
+        //[Theory]
+        //public Task MyTestMethod(bool? mBool)
+        //{
+        //    var b = MyClass.MyBoolMethod(mBool);
+
+        //    Assert.NotNull(b);
+
+        //    return Task.CompletedTask;
+        //}
+
+        //[MemberData(nameof(Strings))]
+        //[Theory]
+        //public async Task MyTestMethodAsync(string text)
+        //{
+        //    var b = await MyClass.MyStringMethodAsync(text);
+
+        //    Assert.NotEmpty(b);
+
+        //    //return Task.CompletedTask;
+        //}
+        //public static IEnumerable<object[]> Strings()
+        //{
+        //    for (var i = 1; i < 11; i++)
+        //    {
+        //        yield return [$"Text Sample {i}"];
+        //    }
+        //}
+    }
+}
